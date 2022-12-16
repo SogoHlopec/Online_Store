74f2e2de2c818eb73de5c38af353fa30260e482f
SogoHlopec <sogo246@gmail.com>
2022-12-16 17:27:06 +0300

    init: change basic settings Webpack, ESlint, TS

--- webpack.config.js
+++ webpack.config.js
@@ -1,58 +1,62 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const FileManagerPlugin = require("filemanager-webpack-plugin");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const ESLintPlugin = require("eslint-webpack-plugin");
-
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FileManagerPlugin = require('filemanager-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const EslingPlugin = require('eslint-webpack-plugin');
 
 module.exports = {
-  entry: path.join(__dirname, "src", "index.js"),
+  entry: path.join(__dirname, 'src', 'index'),
   output: {
-    path: path.join(__dirname, "dist"),
-    filename: "index.js",
-    assetModuleFilename: path.join("images", "[name].[contenthash][ext]"),
+    path: path.join(__dirname, 'dist'),
+    filename: 'index.js',
+    assetModuleFilename: path.join('images', '[name].[contenthash][ext]'),
   },
   module: {
     rules: [
       {
         test: /\.(woff2?|eot|ttf|otf)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
+      { test: /\.ts$/i, use: 'ts-loader' },
       {
         test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, "css-loader"],
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
       },
       {
         test: /\.(png|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
+        type: 'asset/resource',
       },
       {
         test: /\.svg$/,
-        type: "asset/resource",
+        type: 'asset/resource',
         generator: {
-          filename: path.join("icons", "[name].[ext]"),
+          filename: path.join('icons', '[name].[ext]'),
         },
       },
     ],
   },
+  resolve: {
+    extensions: ['.js', '.ts'],
+  },
   plugins: [
+    new EslingPlugin({ extensions: 'ts' }),
     new HtmlWebpackPlugin({
-      template: path.join(__dirname, "src", "index.html"),
-      filename: "index.html",
+      template: path.join(__dirname, 'src', 'index.html'),
+      filename: 'index.html',
     }),
     new FileManagerPlugin({
       events: {
         onStart: {
-          delete: ["dist"],
+          delete: ['dist'],
         },
       },
     }),
     new MiniCssExtractPlugin({
-      filename: "[name].css",
+      filename: '[name].css',
     }),
   ],
   devServer: {
-    watchFiles: path.join(__dirname, "src"),
+    watchFiles: path.join(__dirname, 'src'),
     port: 9000,
   },
 };
