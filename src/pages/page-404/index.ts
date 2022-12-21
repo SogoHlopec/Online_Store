348c9ed8074bf293756b47d2c21cfdf8fa94b1ec
SogoHlopec <sogo246@gmail.com>
2022-12-21 23:38:36 +0300

    feat: add create 404 page

---
+++ src/pages/page-404/index.ts
@@ -0,0 +1,19 @@
+import { CreateElem } from '../../general/CreateElem';
+
+class Create404Page {
+  page404: HTMLElement;
+
+  constructor() {
+    this.page404 = document.createElement('h2');
+  }
+
+  render() {
+    this.page404.innerText = 'PAGE NOT FOUND (404)';
+    const main = document.querySelector('.main');
+    if (main) {
+      main.innerHTML = '';
+      main.prepend(this.page404);
+    }
+  }
+}
+export { Create404Page };
